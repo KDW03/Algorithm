f989a20b6a9e6044872ecde07e3cd417e2f9b0e0
KDW03 <109224863+KDW03@users.noreply.github.com>
2022-10-12 23:25:52 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2344 ms, Memory: 253044 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+    val ownCard  = br.readLine().split(' ').map { it.toInt() }.sorted()
+    val m = br.readLine().toInt()
+    val findCard = br.readLine().split(' ').map { it.toInt() }
+    val sb = StringBuilder()
+    val hashMap = HashMap<Int,Int>()
+
+
+    for (i in ownCard)
+        hashMap[i] = hashMap.getOrDefault(i,0)+1
+    
+    for (i in findCard)
+        sb.append(hashMap.getOrDefault(i,0)).append(" ")
+    
+
+    println(sb.toString())
+}
