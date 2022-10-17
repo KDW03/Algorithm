7ebf5435704f682174f2d8400e3741d7420e5322
KDW03 <109224863+KDW03@users.noreply.github.com>
2022-10-17 22:05:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 12172 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -1,19 +1,13 @@
 fun main() {
     val br = System.`in`.bufferedReader()
-    val t = br.readLine().toInt() //테스트케이스 개수
     val sb = StringBuilder()
-    repeat(t){
+    val dp = IntArray(12)
+    dp[1] = 1;dp[2] = 2;dp[3] = 4
+    for (i in 4..11)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    repeat(br.readLine().toInt()) {
         val n = br.readLine().toInt()
-        val dp = IntArray(12)
-        dp[1] = 1;dp[2] = 2;dp[3] = 4
-        if (n <= 3){
-            sb.append(dp[n]).append("\n")
-        }else{
-            for (i in 4 .. n){
-                dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
-            }
-            sb.append(dp[n]).append("\n")
-        }
+        sb.append(dp[n]).append("\n")
     }
     println(sb.toString())
 }
