095ad2f9820e25fb094c1172728be51131ea8241
KDW03 <109224863+KDW03@users.noreply.github.com>
2022-10-20 22:56:57 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 192 ms, Memory: 29548 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuilder().append("<")
+    val (n,k) = br.readLine().split(' ').map { it.toInt() }
+    val q : Queue<Int> = LinkedList()
+    for (i in 1 .. n)
+        q.add(i)
+    var count = 1
+    while (q.size != 1){
+        val temp = q.poll()
+        if (count == k) {
+            sb.append(temp).append(", ")
+            count = 1
+        }
+        else {
+            count++
+            q.add(temp)
+        }
+    }
+    println(sb.append(q.poll()).append(">").toString())
+}
