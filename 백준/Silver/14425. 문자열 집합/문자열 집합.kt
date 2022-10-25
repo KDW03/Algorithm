c992a6931175b7a027e713437bc5d118a66bdef9
KDW03 <109224863+KDW03@users.noreply.github.com>
2022-10-25 20:59:19 +0900

    [Silver III] Title: 문자열 집합, Time: 392 ms, Memory: 46848 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.kt
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.kt
@@ -1,18 +1,15 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n,m) = br.readLine().split(' ').map { it.toInt() }
-    val first = Array(n){""}
-    val two = Array(m){""}
+    val map = HashMap<String,Int>()
+    for (i in 0 until n){
+        val temp = br.readLine().toString()
+        map[temp] = 1
+    }
     var sum = 0
-    repeat(first.size){
-        first[it] = br.readLine()
+    for (i in 0 until m){
+        val temp = br.readLine()
+        if (map[temp] != null) sum ++
     }
-    repeat(two.size){
-        two[it] = br.readLine()
-    }
-    for (i in  0 until n)
-        sum += two.count { it == first[i] }
    print(sum)
 }
-
-
