ce834a9766aa57eeb5b42e272e7b6075ff023dc7
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-02-28 11:25:05 +0900

    [level 0] Title: 등수 매기기, Time: 29.25 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.kt
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.kt
@@ -1,7 +1,6 @@
 class Solution {
     fun solution(score: Array<IntArray>): IntArray {
-        val sumList = score.map{ it[0] + it[1] }
-        val sumSortList = sumList.sorted().reversed()
-        return sumList.map{ sumSortList.indexOf(it)+1 }.toIntArray()
+        val sumList = score.map{ it.average() }
+        return sumList.map{num -> sumList.count{ it > num } + 1  }.toIntArray()
     }
 }
