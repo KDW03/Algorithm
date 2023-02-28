72a11a9f7d72b512da643914145b9cf6595542b2
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-03-01 01:42:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 8.01 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -0,0 +1,27 @@
+import java.util.*
+class Solution {
+    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        var answer = 1
+        val truckOnBridge : Queue<Int> = LinkedList(List(bridge_length-1){0})
+        var count = 0
+        var idx = 0
+        var curTruck = 0
+        while(true){
+            if(count == truck_weights.size) break
+            if(idx < truck_weights.size 
+               && weight - truckOnBridge.sum() >= truck_weights[idx] 
+               && curTruck < bridge_length){
+                truckOnBridge.add(truck_weights[idx])
+                curTruck++
+                idx++
+            }else{ truckOnBridge.add(0) }
+            val tmp = truckOnBridge.poll()
+            if(tmp > 0) {
+                curTruck --
+                count++
+            }
+            answer++
+        }
+        return answer
+    }
+}
