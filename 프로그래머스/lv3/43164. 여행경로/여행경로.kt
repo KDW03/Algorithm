8418848f24ccec98c58265fce6bae240859ddfa5
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-03-03 22:40:55 +0900

    [level 3] Title: 여행경로, Time: 0.43 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.kt
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.kt
@@ -1,32 +1,33 @@
 import java.util.*
 
 class Solution {
-    private val answerArr: ArrayList<String> = arrayListOf()
-
+    private val answerArr : ArrayList<String> = arrayListOf()
     fun solution(tickets: Array<Array<String>>): Array<String> {
-        val graph: HashMap<String, PriorityQueue<String>> = hashMapOf()
-
-        for (ticket in tickets) {
+        // 연결 그래프 만들고 dfs
+        // 알파벳 순
+        // 총 들려야하는 순서를 저장하는 배열
+        val graph : HashMap<String,PriorityQueue<String>> = hashMapOf()
+        for(ticket in tickets){
             val start = ticket[0]
             val end = ticket[1]
-            if (graph[start] == null) {
+            if(graph[start] == null){
                 graph[start] = PriorityQueue()
             }
             graph[start]!!.add(end)
         }
 
-        dfs(graph, "ICN")
+        dfs(graph,"ICN")
 
         return answerArr.toTypedArray()
     }
 
-    private fun dfs(graph: HashMap<String, PriorityQueue<String>>, start: String) {
-        if (graph[start] != null) {
-            while (graph[start]!!.isNotEmpty()) {
+    private fun dfs(graph:  HashMap<String,PriorityQueue<String>>, start : String){
+        if (graph[start] != null){
+            while(graph[start]!!.isNotEmpty()){
                 val tmp = graph[start]!!.poll()
-                dfs(graph, tmp)
+                dfs(graph,tmp)
             }
         }
-        answerArr.add(0, start)
+        answerArr.add(0,start)
     }
 }
