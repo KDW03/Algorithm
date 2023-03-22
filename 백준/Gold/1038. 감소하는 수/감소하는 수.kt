869feebbc8feaf83e0a61e5826934e1206cf1924
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-03-22 12:53:31 +0900

    [Gold V] Title: 감소하는 수, Time: 128 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.kt
@@ -0,0 +1,22 @@
+var al = ArrayList<Long>()
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+    for (i in 0..9) {
+        dfs(i.toString(), i)
+    }
+    al.sort()
+    if (n >= al.size) {
+        println(-1)
+    }else{
+        println(al[n])
+    }
+}
+
+fun dfs(now: String, last: Int) {
+    al.add(now.toLong())
+    for (i in last - 1 downTo 0) {
+        dfs(now + i.toString(), i)
+    }
+}
