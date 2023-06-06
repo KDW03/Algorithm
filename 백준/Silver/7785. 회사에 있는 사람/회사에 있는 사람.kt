65752076f808dbbe1d9357e9099246b971c5fad8
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-06-06 22:39:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 868 ms, Memory: 72508 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.kt
@@ -2,17 +2,20 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val n = br.readLine().toInt()
     val hashMap = HashMap<String, String>()
+    val sb = StringBuilder()
     repeat(n) {
         val tmp = br.readLine().split(" ")
         hashMap[tmp[0]] = tmp[1]
     }
+    
 
     hashMap.filter {
         it.value == "enter"
     }.map {
         it.key
     }.sortedDescending().forEach {
-        println(it)
+        sb.append(it).append("\n")
     }
 
+    print(sb.toString())
 }
