443e6033a6e06be18af3fcab4fe69d61bc312507
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-06-07 23:11:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.18 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -1,30 +1,38 @@
-import java.util.*
+import java.util.LinkedList
+import java.util.Queue
+
+data class Tr(var weight: Int, var exitTime: Int)
 
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        var answer = 1
-        val truckOnBridge : Queue<Int> = LinkedList()
-        for(i in 0 until bridge_length-1)
-            truckOnBridge.add(0)
-        var count = 0
-        var idx = 0
-        var curTruck = 0
-        while(true){
-            if(count == truck_weights.size) break
-            if(idx < truck_weights.size 
-               && weight - truckOnBridge.sum() >= truck_weights[idx] 
-               && curTruck < bridge_length){
-                truckOnBridge.add(truck_weights[idx])
-                curTruck++
-                idx++
-            }else{ truckOnBridge.add(0) }
-            val tmp = truckOnBridge.poll()
-            if(tmp > 0) {
-                curTruck --
-                count++
+        var nowTime = 0
+        val inWait: Queue<Tr> = LinkedList(truck_weights.map { Tr(it, bridge_length) })
+        val inBridge: Queue<Tr> = LinkedList()
+        val isEnter = BooleanArray(truck_weights.size*bridge_length+1)
+
+        fun canEnter(): Boolean = inBridge.size < bridge_length && !inWait.isEmpty() && (weight - inBridge.sumOf { it.weight }) >= inWait.peek().weight
+
+        fun enterTruck() {
+            nowTime++
+            while (isEnter[nowTime]){
+                nowTime++
             }
-            answer++
+            val enterTruck = inWait.poll().apply {
+                this.exitTime += nowTime
+            }
+            inBridge.offer(enterTruck)
+            isEnter[nowTime] = true
         }
-        return answer
+
+        while (true) {
+            if (inWait.isEmpty() && inBridge.isEmpty()) break
+            while (canEnter()) {
+                enterTruck()
+            }
+            val exitTruck = inBridge.poll()
+            nowTime = (exitTruck.exitTime - 1)
+        }
+        return nowTime + 1
     }
 }
+
