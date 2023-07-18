4fa67ef2e76733141ef5bc5111809fb837507a5e
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-07-18 20:23:50 +0900

    [level 0] Title: 암호 해독, Time: 0.14 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.kt
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(cipher: String, code: Int): String {
-        var answer: String = ""
-        for(i in code-1 until cipher.length step code){
-            answer += cipher[i]  
-        }
-        return answer
-    }
+    fun solution(cipher: String, code: Int): String = cipher.filterIndexed { index, c -> (index+1) % code == 0 }
 }
