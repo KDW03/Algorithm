acb681b29e79c51449c3fb4202334ba22ec6df49
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-06 19:04:36 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 14.47 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -1,29 +1,6 @@
 class Solution {
     fun solution(survey: Array<String>, choices: IntArray): String {
-        survey.forEachIndexed { index, s ->
-            var score = 0
-            var typeClass = Type.RT
-
-            if (s[0] > s[1]) {
-                typeClass = Type.valueOf(s.reversed())
-                score = 8 - choices[index]
-            } else {
-                typeClass = Type.valueOf(s)
-                score = choices[index]
-            }
-
-            if (score <= 3) {
-                typeClass.firstScore += (4 - score)
-            } else if (score >= 5) {
-                typeClass.secondScore += (score - 4)
-            }
-        }
-        return Type.values().map { if (it.firstScore >= it.secondScore) it.name[0] else it.name[1] }.joinToString("")
+        var answer: String = ""
+        return answer
     }
 }
-
-
-enum class Type(var firstScore: Int = 0, var secondScore: Int = 0) {
-    RT, CF, JM, AN
-}
-
