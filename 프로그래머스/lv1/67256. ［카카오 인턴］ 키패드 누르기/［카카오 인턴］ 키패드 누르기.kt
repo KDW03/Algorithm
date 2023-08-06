118d0f33d169c2911ea16663fc73eb689fc375e5
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-06 22:20:36 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 18.94 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.kt
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.kt
@@ -1,37 +1,52 @@
-import kotlin.math.abs
-
 class Solution {
     fun solution(numbers: IntArray, hand: String): String {
-        val keypad = " 123456789*0#"
-        var right = keypad.indexOf('*')
-        var left = keypad.indexOf('#')
+        var answer = ""
+        var right = 0
+        var left = 0
         return numbers.map {
-            val nowHand = when (it) {
+            when (it) {
                 1, 4, 7 -> {
+                    left = it
                     'L'
                 }
                 3, 6, 9 -> {
+                    right = it
                     'R'
                 }
+
                 else -> {
-                    val t = keypad.indexOf(it.digitToChar())
-                    val l = abs(t - left)
-                    val r = abs(t - right)
-                    if ((l / 3) + (l % 3) < (r / 3) + (r % 3)) {
+                    var t = it
+                    var r = 0
+                    var l = 0
+
+                    if (it == 0){
+                        t = 8
+                    }
+
+                    if (right == 0){
+                        right = 8
+                        r++
+                    }
+
+                    if (left == 0){
+                        left = 8
+                        l++
+                    }
+
+                    l = t - left
+                    r = t - right
+
+                    if ((l / 3) + (l % 3) > (r / 3) + (r % 3)) {
+                        left = it
                         'L'
-                    } else if ((l / 3) + (l % 3) > (r / 3) + (r % 3)) {
+                    } else if ((l / 3) + (l % 3) < (r / 3) + (r % 3)) {
+                        right = it
                         'R'
                     } else {
-                        if (hand == "right") {
-                            'R'
-                        } else {
-                            'L'
-                        }
+                        if (hand == "right") 'R' else 'L'
                     }
                 }
             }
-            if (nowHand == 'R') right = keypad.indexOf(it.digitToChar()) else left = keypad.indexOf(it.digitToChar())
-            nowHand
         }.joinToString("")
     }
 }
