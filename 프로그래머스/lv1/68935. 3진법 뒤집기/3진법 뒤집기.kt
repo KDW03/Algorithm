e9b275a6fae6c271fdde64e03cf01f4944a8c41c
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-08 23:13:20 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.01 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -1,19 +1,4 @@
-import kotlin.math.*
 
 class Solution {
-    fun solution(n: Int): Int {
-        val sb = StringBuilder()
-        var s = n
-        var result = 0.0
-        while(s > 0){
-            sb.append(s % 3)
-            s /= 3
-        }
-        val t = sb.toString()
-        t.mapIndexed { i , v -> 
-            val digit = t.length - i - 1
-            result += (3.0.pow(digit) * v.toString().toDouble())
-        }        
-        return result.toInt()
-    }
+    fun solution(n: Int): Int = n.toString(3).reversed().toInt(3)
 }
