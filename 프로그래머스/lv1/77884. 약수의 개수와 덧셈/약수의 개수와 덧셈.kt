7c6e649c4336ffec1290368b1a33d343dafb5b74
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-08 23:59:06 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 8.73 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,17 +1,6 @@
 import kotlin.math.*
 
 class Solution {
-    fun solution(left: Int, right: Int): Int {
-        return (left .. right).map {
-            isPlus(it)       
-        }.sum()
+    fun solution(left: Int, right: Int): Int = (left..right).map{ i -> if( (1..i).filter { i % it == 0 }.size % 2 == 0) i else -i }.sum()
     }
-}
 
-fun isPlus(num : Int) : Int {
-    var count = 2
-    val a = sqrt(num.toDouble()).toInt()
-    for(i in 2 until a) if(num % i == 0) count += 2 
-    if(num == a.toDouble().pow(2.0).toInt() && num % a == 0) count ++  
-    return if(count % 2 == 0) num else -num
-}
