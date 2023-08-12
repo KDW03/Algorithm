eb83f19c28b7f8547118dd7ace9b1da0fc152713
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-12 23:36:59 +0900

    [unrated] Title: 추억 점수, Time: 26.13 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.kt
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.kt
@@ -2,12 +2,10 @@ class Solution {
 
     fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
 
-        val map = name.mapIndexed { index, name ->
-            Pair(name, yearning[index])
-        }.toMap()
-
+        val map = name.zip(yearning.toTypedArray()).toMap()
+        
         return photo.map { persons ->
-            persons.sumOf { person -> if (map.contains(person)) map[person]!! else 0 }
+            persons.sumOf { person -> map[person] ?: 0 }
         }.toIntArray()
         
     }
