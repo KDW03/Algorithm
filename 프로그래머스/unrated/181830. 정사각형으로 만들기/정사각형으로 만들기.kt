5b1aee4ea04ec730bf9f9e09def2d3aa9cf0ae48
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-17 17:44:26 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.52 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
@@ -2,18 +2,14 @@ class Solution {
     fun solution(arr: Array<IntArray>): Array<IntArray> {
         val rowSize = arr.size
         val colSize = arr[0].size
-        return if (colSize > rowSize) {
-            var t = arr
-            repeat(colSize - rowSize){
-                t += IntArray(colSize)
+        val size = maxOf(rowSize,colSize)
+        
+        val answer : Array<IntArray> = Array(size){ IntArray(size) }
+        for(i in 0 until rowSize){
+            for(j in 0 until colSize){
+                answer[i][j] = arr[i][j]
             }
-            t
-        } else if (rowSize > colSize) {
-            arr.map {
-                IntArray(rowSize) { idx -> if (colSize > idx) it[idx] else 0 }
-            }.toTypedArray()
-        } else {
-            arr
         }
+        return answer
     }
 }
