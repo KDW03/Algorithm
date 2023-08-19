2353457a2f09479e5ada7d3220cfa202ae0358b8
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-19 15:23:12 +0900

    [level 1] Title: 체육복, Time: 19.68 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.kt
+++ 프로그래머스/lv1/42862. 체육복/체육복.kt
@@ -3,23 +3,12 @@ class Solution {
         var answer = n - lost.size
 
         var mr = reserve.toMutableList()
-        var ml = lost.toMutableList()
-        
-        lost.forEach {
-            if (mr.remove(it)) {
-                ml.remove(it)
-                answer++
-            }
-        }
-        
-        ml.sorted().forEach {
-            if (it - 1 > 0 && mr.remove(it - 1)) {
-                answer++
-            } else if (it + 1 <= n && mr.remove(it + 1)) {
-                answer++
-            }
-        }
 
+        lost.forEach {
+            if (it - 1 >= 0 && mr.remove(it - 1)) answer++
+            else if (it + 1 <= n && mr.remove(it + 1)) answer++
+        }
+        
         return answer
     }
 }
