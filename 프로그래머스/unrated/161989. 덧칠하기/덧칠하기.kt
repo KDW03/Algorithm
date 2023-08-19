fe864f5b0296591484aa23f3d7da39206dc00ed4
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2023-08-19 22:33:48 +0900

    [unrated] Title: 덧칠하기, Time: 1.27 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.kt
@@ -1,26 +1,13 @@
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
-
-        var value = section.first() + m - 1
-        var answer = 1
-        var start = 0
-
-        while (true) {
-            
-            val idx = section.binarySearch(value, start).let {
-                if (it < 0) -it - 1 else it + 1
-            }
-         
-            if (idx >= section.size) {
-                break
-            } else {
-                value = section[idx] + m - 1
-                start = idx
+        var answer = 0
+        var nextSection = 0
+        section.forEach {        
+            if (it >= nextSection) {
                 answer++
+                nextSection = it + m
             }
         }
-        
         return answer
-
     }
 }
