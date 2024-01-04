6a6b338e2944532b099ce1517b733d98fd6d8c70
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-01-05 00:01:51 +0900

    [Silver IV] Title: 로프, Time: 356 ms, Memory: 26420 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.kt
+++ 백준/Silver/2217. 로프/로프.kt
@@ -1,16 +1,20 @@
-fun main(){
+fun main() {
     val br = System.`in`.bufferedReader()
     val n = br.readLine().toInt()
     val arr = IntArray(n)
-    for(i in 0 until n){
-        arr[i] = br.readLine().toInt()
+
+    repeat(n) {
+        arr[it] = br.readLine().toInt()
     }
-    var max = 0
-    for((i,value) in arr.sorted().withIndex()){
-        val tmp = value * (n-i)
-        if(tmp > max){
-            max = tmp
+
+    arr.sortDescending()
+    var answer = arr[0]
+
+    for ((i,v) in arr.withIndex()) {
+        val new = v  * (i + 1)
+        if (new >= answer) {
+            answer = new
         }
     }
-    println(max)
+    println(answer)
 }
