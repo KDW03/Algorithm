326c108e225b7eae7ac3bc9f21954443e3366532
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-01-10 16:54:37 +0900

    [Silver II] Title: 나무 자르기, Time: 708 ms, Memory: 209060 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.kt
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.kt
@@ -1,22 +1,21 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-
     val arr = br.readLine().split(" ").map { it.toLong() }
 
     var start = 1L
-    var end = arr.max().toLong()
+    var end = arr.max()
 
 
     while (start <= end) {
         val middle = (start + end) / 2
-
-        val sum = arr.sumOf {
-            if (it > middle) {
-                it - middle
-            } else {
-                0
+        var sum = 0L
+        
+        for (tree in arr) {
+            if (tree > middle) {
+                sum += tree - middle
             }
+            if (sum >= m) break 
         }
 
         if (sum >= m) {
