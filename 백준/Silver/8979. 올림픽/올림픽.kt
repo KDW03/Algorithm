858164845296b80e360cb5f73987a4898e9916bc
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-01-26 02:56:31 +0900

    [Silver V] Title: 올림픽, Time: 184 ms, Memory: 21828 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.kt
+++ 백준/Silver/8979. 올림픽/올림픽.kt
@@ -1,13 +1,19 @@
+data class Country(val number: Int, val gold: Int, val silver: Int, val bronze: Int)
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    val arr = Array(n) { "" }
+    val countries = ArrayList<Country>()
+
     repeat(n) {
-        val tmp = br.readLine().split(" ").joinToString("")
-        val idx = tmp.first()
-        val str = tmp.drop(1)
-        arr[idx.digitToInt() - 1] = str
+        val (num, gold, silver, bronze) = br.readLine().split(" ").map { it.toInt() }
+        countries.add(Country(num, gold, silver, bronze))
     }
-    val find = arr[k - 1]
-    println(arr.sortedDescending().indexOfFirst { it == find } + 1)
+    val find = countries.find { it.number == k }
+    countries.sortWith(compareByDescending<Country> { it.gold }.thenByDescending { it.silver }
+        .thenByDescending { it.bronze })
+
+    val rank =
+        countries.indexOfFirst { it.gold == find?.gold && it.silver == find.silver && it.bronze == find.bronze } + 1
+    println(rank)
 }
