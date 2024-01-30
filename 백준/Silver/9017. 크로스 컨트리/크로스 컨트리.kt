4675357aeeba71c6b717e3d5aa2d5c1740a21a29
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-01-30 20:42:31 +0900

    [Silver III] Title: 크로스 컨트리, Time: 404 ms, Memory: 33012 KB -BaekjoonHub

---
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuilder()
+    val n = br.readLine().toInt()
+    repeat(n) {
+        val c = br.readLine().toInt()
+        val countValueMap = hashMapOf<Int, Triple<Int, Int, Int>>()
+        val result = br.readLine().split(" ").map { it.toInt() }
+        val rankTeam = result.groupingBy { it }.eachCount().filter { it.value >= 6 }.keys
+        var score = 0
+        result.filter { rankTeam.contains(it) }.forEach {
+            score++
+            countValueMap[it] = countValueMap.getOrDefault(it, Triple(0, 0, Int.MAX_VALUE)).let { pre ->
+                if (pre.first < 4) {
+                    Triple(pre.first + 1, pre.second + score, Int.MAX_VALUE)
+                } else if (pre.first == 4 && pre.third == Int.MAX_VALUE) {
+                    Triple(pre.first, pre.second, score)
+                } else {
+                    pre
+                }
+            }
+        }
+        val s = countValueMap.toList().sortedWith(compareBy({ it.second.second }, { it.second.third }))
+        sb.append(s.first().first).append("\n")
+    }
+    print(sb.toString())
+}
