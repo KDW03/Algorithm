91944e79ee3c2bedf41ca06d9652ce86b2eb3fa4
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-02-08 00:45:58 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.13 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -1,17 +1,6 @@
 class Solution {
-   // 시간복잡도 : arr1 x * arr2 x * arr2 y
     fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        val result = Array(arr1.size) { IntArray(arr2[0].size) { 0 } }
-
-        for (i in arr1.indices) {
-            for (j in arr2[0].indices) {
-                // arr2의 행, arr1의 열에 대해서 반복합니다.
-                for (k in arr2.indices) {
-                    result[i][j] += arr1[i][k] * arr2[k][j]
-                }
-            }
-        }
-
-        return result
+        var answer = arrayOf<IntArray>()
+        return answer
     }
 }
