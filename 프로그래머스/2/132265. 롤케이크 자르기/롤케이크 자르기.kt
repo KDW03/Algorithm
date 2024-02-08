e212c9b71e9b5f740e5af76c645808fc88c9de68
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-02-08 23:01:54 +0900

    [level 2] Title: 롤케이크 자르기, Time: 286.24 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -1,17 +1,19 @@
 class Solution {
     fun solution(topping: IntArray): Int {
-        val totalToppings = topping.groupBy { it }.mapValues { it.value.size }.toMutableMap()
-        val currentToppings = mutableMapOf<Int, Int>()
-        var count = 0
 
-        topping.dropLast(1).forEach { top ->
-            currentToppings[top] = currentToppings.getOrDefault(top, 0) + 1
-            totalToppings[top] = totalToppings[top]!! - 1
-            if (totalToppings[top] == 0) totalToppings.remove(top)
+        val aCountMap : MutableMap<Int,Int> = hashMapOf()
+        val bCountMap = topping.toList().groupingBy{ it }.eachCount().toMutableMap()
+        var answer = 0
 
-            if (currentToppings.size == totalToppings.size) count++
+        // n 
+        for(i in 0 until topping.size - 1) {
+            val top = topping[i]
+            aCountMap[top] = aCountMap.getOrDefault(top,0) + 1
+            val bNew = bCountMap[top]!! - 1
+            if(bNew == 0) bCountMap.remove(top) else bCountMap[top] = bNew
+            if(aCountMap.size == bCountMap.size) answer++
         }
 
-        return count
+        return answer
     }
 }
