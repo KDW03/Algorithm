91afdd1eaba9b79ea64a7a319d650d0c6b265ee6
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-02-08 23:37:11 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.97 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.kt
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.kt
@@ -1,25 +1,42 @@
 import java.util.*
 
 class Solution {
-    fun solution(x: Int, y: Int, n: Int): Int {
-        if (x > y) return -1
+    fun solution(x: Int, y: Int, n: Int): Int {        
+        
 
-        val queue: Queue<Pair<Int, Int>> = LinkedList()
-        val visited = HashSet<Int>()
-        queue.add(Pair(x, 0))
-
-        while (queue.isNotEmpty()) {
-            val (current, count) = queue.poll()
-
-            if (current == y) return count
-            if (current > y || visited.contains(current)) continue
-
-            visited.add(current)
-            queue.add(Pair(current + n, count + 1))
-            queue.add(Pair(current * 2, count + 1))
-            queue.add(Pair(current * 3, count + 1))
+        val q : Queue<Pair<Int,Int>> = LinkedList()
+        q.add(Pair(x,0))
+        val visited = BooleanArray(y + 1)
+        
+        while(q.isNotEmpty()) {
+            val (now,cost) = q.poll()
+            if(now == y) return cost
+            
+            val first = now + n
+            if(first <= y) {
+                if(!visited[first]) {
+                    q.add(Pair(first, cost + 1))
+                    visited[first] = true
+                }
+            }
+            
+            val second = 2 * now
+            if(second <= y)  {
+                if(!visited[second]) {
+                    q.add(Pair(second, cost + 1))
+                    visited[second] = true
+                }
+            }
+            
+            val third = 3 * now
+            if(third <= y) {
+                if(!visited[third]) {
+                    q.add(Pair(third, cost + 1))
+                    visited[third] = true
+                }
+            }
         }
-
+        
         return -1
     }
 }
