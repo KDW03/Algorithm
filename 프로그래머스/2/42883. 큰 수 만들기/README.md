a2bb25166a9f1be6d852c8d94b5a0200b1b5190a
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-02-17 22:45:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 15.38 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
 
 ### 성능 요약
 
-메모리: 59.6 MB, 시간: 1.22 ms
+메모리: 62.6 MB, 시간: 15.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 22:33:15
+2024년 2월 6일 22:45:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.kt
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.kt
@@ -1,28 +1,16 @@
+import java.util.*
 class Solution {
     fun solution(number: String, k: Int): String {
-        var windowSize = k
-        val ans = StringBuilder()
-        var count = 0
-        var i = 0
-        outer@while(i in number.indices) {
-            val now = number[i].digitToInt()
-            // now 다음부터 윈도우 size만큼 확인
-            for(j in i + 1 .. i + windowSize) {
-                // 윈도우내에 더 큰 숫자가 있다면
-                // 윈도우 사이즈 줄이고 now 제거
-                if(number[j].digitToInt() > now) {
-                    windowSize -= (j - i)
-                    i += (j - i)
-                    continue@outer
-                }
+        val stack = Stack<Char>()
+        var count =0
+        for(c in number) {
+            while(stack.isNotEmpty() && count < k &&c > stack.peek()) {
+                stack.pop()
+                count++
             }
-            // 제거 안됐다면 정답에 포함
-            ans.append(now)
-            count++
-            i++
-            if(count >= number.length - k) break@outer
+            stack.add(c)
         }
         
-        return ans.toString()
+        return stack.toList().joinToString("").take(number.length - k)
     }
 }
