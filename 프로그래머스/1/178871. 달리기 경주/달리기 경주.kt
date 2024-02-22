3bd916d7870463ccb927b30864daf9dce4bda8ce
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-02-22 13:05:08 +0900

    [level 1] Title: 달리기 경주, Time: 2.67 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
@@ -1,23 +1,30 @@
 class Solution {
     fun solution(players: Array<String>, callings: Array<String>): Array<String> {
-        val indexMap = players.withIndex().associate { it.value to it.index }.toMutableMap()
 
-        callings.forEach {
-            val i = indexMap[it] ?: return@forEach
-            val j = i - 1
-            overtaking(players, indexMap, i, j)
+
+        val map = players.mapIndexed { i , v ->
+            v to i
+        }.toMap().toMutableMap()
+
+        val answer = players
+
+
+        fun swap(callingIdx : Int) {
+            if(callingIdx == 0) return
+            val tmp = answer[callingIdx - 1]
+            answer[callingIdx - 1] = answer[callingIdx]
+            answer[callingIdx] = tmp
+            // map 순서 변경
+            map[answer[callingIdx]] = map[answer[callingIdx]]!! + 1
+            map[answer[callingIdx - 1]] = map[answer[callingIdx - 1]]!! - 1
         }
 
-        return players
-    }
 
-    fun overtaking(players: Array<String>, indexMap: MutableMap<String, Int>, i: Int, j: Int) {
-        val temp = players[j]
-        players[j] = players[i]
-        players[i] = temp
+        callings.forEach {
+            swap(map.getOrDefault(it,0))
+        }
 
-        indexMap[players[j]] = j
-        indexMap[players[i]] = i
+
+        return answer
     }
 }
-
