2b9b90ec0ea5c9ec97cd10ad6270c628dbc5091b
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-03-01 23:37:06 +0900

    [Gold II] Title: 교환, Time: 312 ms, Memory: 51140 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.kt
+++ 백준/Gold/1039. 교환/교환.kt
@@ -1,7 +1,6 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-
     val nums = n.toString().toCharArray().map { it.digitToInt() }.toIntArray()
     val visited = Array(11) { BooleanArray(1000001) }
     val answer = dfs(nums, k, visited)
@@ -11,11 +10,11 @@ fun main() {
 fun dfs(nums: IntArray, k: Int, visited: Array<BooleanArray>, cost: Int = 0): Int {
     var max = Int.MIN_VALUE
     val size = nums.size
-    // i 와 j 를 바꾼다.
+    
     val now = nums.joinToString("").toInt()
     if (visited[cost][now]) return max
     visited[cost][now] = true
-    if (cost == k) return nums.joinToString("").toInt()
+    if (cost == k) return now
 
     for (i in 0 until size - 1) {
         for (j in i + 1 until size) {
@@ -27,7 +26,6 @@ fun dfs(nums: IntArray, k: Int, visited: Array<BooleanArray>, cost: Int = 0): In
     }
     return max
 }
-
 
 fun swap(nums: IntArray, i: Int, j: Int) {
     val tmp = nums[i]
