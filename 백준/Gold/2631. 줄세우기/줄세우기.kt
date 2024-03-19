c03dd406bc10f77c01d1f2ccb75fc9e533256414
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-03-19 20:26:11 +0900

    [Gold IV] Title: 줄세우기, Time: 108 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.kt
+++ 백준/Gold/2631. 줄세우기/줄세우기.kt
@@ -1,17 +1,21 @@
 fun main() {
     val br = System.`in`.bufferedReader()
-
     val n = br.readLine().toInt()
     val nums = Array(n) { br.readLine().toInt() }
 
-    val dp = IntArray(n) { 1 }
-    for (i in 1 until n) {
-        val a = nums[i]
-        for (j in 0 until i) {
-            val b = nums[j]
-            if (a > b) dp[i] = maxOf(dp[i], dp[j] + 1)
+    val lisCandidates = ArrayList<Int>()
+
+    for (num in nums) {
+        val pos = lisCandidates.binarySearch(num)
+        if (pos < 0) {
+            val insertPos = -(pos + 1)
+            if (insertPos == lisCandidates.size) {
+                lisCandidates.add(num)
+            } else {
+                lisCandidates[insertPos] = num
+            }
         }
     }
 
-    println(n - dp.maxOrNull()!!)
+    println(n - lisCandidates.size)
 }
