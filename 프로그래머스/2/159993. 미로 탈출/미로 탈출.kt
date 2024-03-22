30c9106714bb77c4cfa5cf950ae84e2dc23fdf3b
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-03-22 13:45:39 +0900

    [level 2] Title: 미로 탈출, Time: 0.85 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.kt
@@ -1,71 +1,74 @@
 import java.util.*
 
-data class Position(val x : Int, val y : Int)
+data class Point(val x : Int = 0, val y : Int = 0)
 
 class Solution {
     fun solution(maps: Array<String>): Int {
-        // 출발점 -> 레버 bfs
-        var startPoint = Position(0,0)
-        var laberPoint = Position(0,0)
-        var exitPoint = Position(0,0)
+        var answer: Int = 0
         
-        maps.forEachIndexed { x , v ->
+        var start = Point()
+        var lever = Point()
+        var exit = Point()
+        
+        
+        maps.forEachIndexed { x,v ->
             v.forEachIndexed { y, c ->
-                
-                if(c == 'S') {
-                    startPoint = Position(x,y)
-                }
-                
-                if(c == 'L') {
-                    laberPoint = Position(x,y)
-                }
-                
-                if(c == 'E') {
-                    exitPoint = Position(x,y)
-                }
-                
-            }
-        }
-        
-        
-        val moveX = arrayOf(0,0,-1,1)
-        val moveY = arrayOf(1,-1,0,0)
-        
-        fun bfs(a : Position, b : Position) : Int {
-            
-            val visited = Array(maps.size) { BooleanArray(maps[0].length) }
-            val q : Queue<Pair<Position,Int>> = LinkedList()
-            
-            q.add(Pair(a,0))
-            visited[a.x][a.y] = true
-            
-            while(q.isNotEmpty()) {
-                val (p,cost) = q.poll()
-                val (x,y) = p
-                
-                if(p == b) return cost
-                
-                for(i in 0 until 4) {
-                    val nx = x + moveX[i]
-                    val ny = y + moveY[i]
-                    
-                    if(nx in maps.indices && ny in maps[0].indices && !visited[nx][ny] && maps[nx][ny] != 'X') {
-                        visited[nx][ny] = true
-                        q.add(Pair(Position(nx,ny),cost + 1))
+                when(c) {
+                    'S' -> {
+                        start = Point(x,y)
+                    }
+                    'L' -> {
+                        lever = Point(x,y)
+                    }
+                    'E' -> {
+                        exit = Point(x,y)
                     }
                 }
             }
+        }
+        
+        val moveX = arrayOf(0,0,-1,1)
+        val moveY = arrayOf(1,-1,0,0)
+
+        var distMap = Array(maps.size) { IntArray(maps[0].length){ 10001 } }
+        distMap[start.x][start.y] = 0
+        
+        fun bfs(start : Point, find : Point) : Int {
+            val q : Queue<Pair<Point,Int>> = LinkedList()
             
+            q.add(Pair(start,0))
+            
+            while(q.isNotEmpty()) {
+                val (p,cost) = q.poll()
+                
+                if(p == find) {
+                    return cost
+                }
+                
+                for(i in 0 until 4) {
+                    val nx = p.x + moveX[i]
+                    val ny = p.y + moveY[i]
+                    val newCost = cost + 1
+                    if(nx in maps.indices && ny in maps[0].indices && maps[nx][ny] != 'X' && distMap[nx][ny] > newCost) {
+                        distMap[nx][ny] = newCost
+                        q.add(Pair(Point(nx,ny),newCost))
+                    }
+                }
+            }
             return -1
         }
         
         
+        // 시작점 To 레버
+        val mDistToLever = bfs(start = start, find = lever)
+        if(mDistToLever == -1) return -1
         
-        val toLCost = bfs(startPoint,laberPoint)
-        if(toLCost == -1) return -1
-        val toECost = bfs(laberPoint,exitPoint)
-        if(toECost == -1) return -1
+        // 레버 To 출구
+        distMap = Array(maps.size) { IntArray(maps[0].length){ 10001 } }
+        val mDistToEixt = bfs(start = lever, find = exit)
+        if(mDistToEixt == -1) return -1
         
-        return toLCost + toECost
+        
+        return mDistToLever + mDistToEixt
     }
 }
