f27346765738bbc8a5511525b97342f6cb5b31bd
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-03-31 19:17:47 +0900

    [Gold II] Title: 새로운 게임 2, Time: 176 ms, Memory: 23136 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.kt
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.kt
@@ -1,7 +1,6 @@
 import java.util.*
 import kotlin.collections.ArrayList
 
-// →, ←, ↑, ↓
 val moveX = arrayOf(0, 0, -1, 1)
 val moveY = arrayOf(1, -1, 0, 0)
 
@@ -14,7 +13,6 @@ data class Piece(val idx: Int, var x: Int, var y: Int, var dir: Int) {
         if (nx in boardColor.indices && ny in boardColor.indices && boardColor[nx][ny] != 2) {
             val tmp = ArrayList<Int>()
 
-            // 만약 idx에 일치하는거 나올때까지
             while (board[x][y].isNotEmpty()) {
                 val id = board[x][y].pop()
                 tmp.add(id)
@@ -28,7 +26,6 @@ data class Piece(val idx: Int, var x: Int, var y: Int, var dir: Int) {
                     board[nx][ny].add(id)
                 }
             } else {
-                // 빨간색 뒤집기
                 for (id in tmp) {
                     pieces[id].x = nx
                     pieces[id].y = ny
@@ -37,13 +34,9 @@ data class Piece(val idx: Int, var x: Int, var y: Int, var dir: Int) {
             }
             if (board[nx][ny].size >= 4) return false
         } else {
-            // 범위에 벗어나거나 파란색이라면
-            // 방향 바꿔서 가보기
             changeDir()
-
             val nnx = x + moveX[dir]
             val nny = y + moveY[dir]
-
             if (nnx in boardColor.indices && nny in boardColor.indices && boardColor[nnx][nny] != 2) {
                 return move(boardColor, board, pieces)
             }
@@ -63,17 +56,12 @@ data class Piece(val idx: Int, var x: Int, var y: Int, var dir: Int) {
 
 fun main() {
     val br = System.`in`.bufferedReader()
-
-    // N 체스판 크기, K 말의 개수
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    // 0은 흰색, 1은 빨간색, 2는 파란색
-    var boardColor: Array<IntArray> = Array(n) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
+    val boardColor: Array<IntArray> = Array(n) { br.readLine().split(" ").map { it.toInt() }.toIntArray() }
     val board: Array<Array<Stack<Int>>> = Array(n) { Array(n) { Stack<Int>() } }
     val pieces: Array<Piece> = Array(k) { idx ->
         val (x, y, dir) = br.readLine().split(" ").map { it.toInt() - 1 }
-        // 스택에 넣기
         board[x][y].add(idx)
-        // 돌아가면서 해당 위치에 나올때
         Piece(idx, x, y, dir)
     }
 
@@ -82,13 +70,8 @@ fun main() {
         for (p in pieces) {
             if (!p.move(boardColor,board,pieces)) break@outer
         }
-
         turnCount++
     }
 
-    if (turnCount > 1000) {
-        println(-1)
-    } else {
-        println(turnCount)
-    }
+    if (turnCount > 1000) println(-1) else println(turnCount)
 }
