92b076d539296eee3f59a7f5e1ac2c8d99e6079a
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-04-15 21:59:28 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 19.42 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(s: String): String {
-        return s.split(" ").map { 
-            if(!it.isEmpty() && !it.first().isDigit()) {
-                it.first().uppercase() + it.drop(1).lowercase()
-            }else {
-                it.lowercase()
-            }
-        }.joinToString(" ")
+        return s.split(" ").map { if(it.isBlank()) "" else it.first().uppercase() + it.drop(1).lowercase() }.joinToString(" ")
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 62.5 MB, 시간: 16.83 ms
+메모리: 62 MB, 시간: 19.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 12:3:54
+2024년 04월 15일 21:59:27
 
 ### 문제 설명
 
