c89a545c095cacc6aa9d1109b668d85e06377944
Dowoo Kim <109224863+KDW03@users.noreply.github.com>
2024-04-16 23:52:32 +0900

    [level 2] Title: 피보나치 수, Time: 2.74 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.kt
@@ -1,15 +1,19 @@
 class Solution {
     fun solution(n: Int): Int {
-        val dp = IntArray(n + 1) { -1 }
-        dp[0] = 0
-        dp[1] = 1
-        return fibo(n,dp)
+    val MOD = 1234567
+    if (n == 2) return 1
+
+    var a = 0L  
+    var b = 1L  
+    var c = 0L
+
+    for (i in 2..n) {
+        c = (a + b) % MOD  
+        a = b
+        b = c
     }
-    
-    
-    fun fibo(n : Int, dp : IntArray) : Int {
-        if(dp[n] == -1) dp[n] = (fibo(n - 1,dp) + fibo(n - 2,dp)) % 1234567
-        return dp[n]
-    }
-    
+
+    return c.toInt()
+}
+
 }
